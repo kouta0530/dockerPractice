43c6b81b8ef1dc4c2217f235dcd4a1a7dcd18df0
kouta0530 <16jk047@ymklab.net>
2021-07-06 23:09:02 +0900

    feat(lamp): phpとApache環境のDockerコンテナを作ってみた

---
+++ lamp/php/html/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>php7.2-apache</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<?php
+    echo 'Hello world.';
+?>
+</body>
+</html>mysql5.7-trial2
